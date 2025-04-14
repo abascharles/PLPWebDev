159a009f209019209d60da38b4aa21f3ac0894d6
abascharles <abascharlesbenard@gmail.com>
2025-04-14 08:46:30 +0300

    Property acesseors, events and fucntions

--- week5/script.js
+++ week5/script.js
@@ -1,1 +1,10 @@
+let button = document.querySelector("button");
+let box = document.getElementById("changeMe");
 
+button.onclick = function changeColor() {
+  if (box.style.background == "red") {
+    box.style.background = "blue";
+  } else {
+    box.style.background = "red";
+  }
+};
