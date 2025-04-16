e687893a0e8784a9e644725915c70f8b3a075acc
abascharles <abascharlesbenard@gmail.com>
2025-04-16 07:17:05 +0300

    The  DOM

---
+++ week5/DOM.js
@@ -0,0 +1,6 @@
+// DOM = DOCUMENT OBJECT MODEL
+//       Object{} that represents the page  that you see in the web browser and
+//       provides you with an API to interact with it.
+//       The web browse constructs the DOM when it loads the HTML document,
+//       and structures all the elements ina tree-like format.
+//       Javascript can access and manipulate the DOM to dynamically change the content, structure, and style of a web page.
