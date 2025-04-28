95537d72842b4d0fe5f3f92eef74d7e062ed4c99
abascharles <abascharlesbenard@gmail.com>
2025-04-28 19:25:31 +0300

    Ways of Aceesing elements

--- week6/dom.js
+++ week6/dom.js
@@ -1,4 +1,14 @@
 //Accessing DOM elements
 //By ID
-const elementID = document.getElementById("elementID");
+const elementID = document.getElementById("elementID");\
+
 //By class name
+const byClassName = document.getElementsByClassName("className"); 
+
+//By tag name
+const byTagName = document.getElementsByTagName("tagName");
+
+//By query selector(Itas like cessing them by css selector)
+// Selects the first element that matches the selector
+const byQuerySelectoClass = document.querySelector(".className");  // Selects the first element with the class name
+const byQuerySelectorID = document.querySelectorAll("#className"); // Selects all elements with the class name
